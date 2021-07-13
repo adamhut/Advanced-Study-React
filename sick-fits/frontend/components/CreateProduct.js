21e43551ac88f4fa890a94bedbd6524bf038b1c4
adamhut <adamhut@gmail.com>
2021-07-13 09:58:31 -0700

    wip add data ep 24

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,21 +1,66 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from './../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+      # which variables are getting passed in? and what types are they.
+      $name: String!
+      $description: String!
+      $price: Int!
+      $image: Upload
+  ){
+    createProduct(
+      data:{
+        name: $name
+        description:$description
+        price: $price
+        status: "AVAILABLE",
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+    }
+  }
+`;
+
 
 export default function CreateProduct() {
-    const { inputs, handleChange, clearForm, resetForm } = useForm({
-        image: '',
-        name: 'Nice shoe',
-        price: 323432,
-        description: 'description'
-    });
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
+    name: 'Nice shoe',
+    price: 3432,
+    description: 'description'
+  });
 
-    function handleSubmit() {
-
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs
     }
+  );
+//   console.log(createProduct);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
     return (
-        <Form onSubmit={ handleSubmit }>
-            <fieldset>
+        <Form
+            onSubmit={async (e) => {
+                e.preventDefault();
+                console.log(inputs);
+                //submit the inputfields to the backend:
+                const res = await createProduct();
+                //console.log(res)
+                clearForm();
+            }}>
+            <DisplayError error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
                 <label htmlFor="image">
                     Image
                     <input
